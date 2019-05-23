2df29a98916d655cc8ad4935f2687f9035b60aef
Arturo <grabowski.a93@gmail.com>
2019-05-23 18:11:33 +0200

    import/export render to website to 13(included)

---
+++ notetoself/src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class App extends Component{
+    render() {
+        return (
+            <div>
+                <h2> Note to Self 2 </h2>
+            </div>
+        )
+    }
+}
+/*
+export const color = 'blue';
+export const number = 22;*/
+
+export default App;
