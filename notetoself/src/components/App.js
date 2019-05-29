f29c350e76e16fa46ef4593116acaa685db0a284
Arturo <grabowski.a93@gmail.com>
2019-05-29 19:40:17 +0200

    Small formule 'to do' with button and formcontrol ->16 episode

--- notetoself/src/components/App.js
+++ notetoself/src/components/App.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react';
+import {Form,FormControl, Button} from 'react-bootstrap'; //it's not a default that's why the brackets are here!
 
 class App extends Component{
+    constructor() {
+        super();
+
+        this.state = {
+            text: ''
+        }
+    }
+
     render() {
         return (
             <div>
                 <h2> Note to Self 2 </h2>
+                <Form inline> {/*to to samo co by bylo : inline={true} */}
+                    <FormControl onChange={event => this.setState({ text : event.target.value})}/> {/*Here user will
+                     put a data and it has not any data or value. UWAGA NA {}*/}
+                    {' '}
+                    <Button onClick={() => console.log(this.state)}>Submit </Button>
+                </Form>
             </div>
         )
     }
 }
-/*
-export const color = 'blue';
-export const number = 22;*/
+
 
 export default App;
